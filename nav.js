90445e131c698670d4e4e64e08df97f8b0148306
avaphan <aphan2@iastate.edu>
2026-09-09 14:20:41 -0500

    Refactor navigation structure and update links

--- nav.js
+++ nav.js
@@ -1,64 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Find the navigation area in the HTML
     const navigation =
         document.getElementById("navigation");
 
 
-    // If the page does not contain the navigation area,
-    // stop the script
     if (!navigation) {
         return;
     }
 
 
-    // Add the navigation menu
     navigation.innerHTML = `
-        <nav class="main-navigation">
+        <nav class="main-nav">
 
-            <a href="index.html">
-                About Me
-            </a>
+            <div class="nav-brand">
 
-            <a href="research.html">
-                Research
-            </a>
+                <a href="index.html">
+                    Ava Phan
+                </a>
 
-            <a href="personal.html">
-                Personal
-            </a>
+            </div>
+
+
+            <div class="nav-links">
+
+                <a href="index.html">
+                    About Me
+                </a>
+
+                <a href="research.html">
+                    Research
+                </a>
+
+                <a href="personal.html">
+                    Personal
+                </a>
+
+            </div>
 
         </nav>
     `;
 
 
-    // Find the name of the current page
     let currentPage =
         window.location.pathname.split("/").pop();
 
 
-    // GitHub Pages may show the homepage
-    // without displaying index.html
     if (currentPage === "") {
         currentPage = "index.html";
     }
 
 
-    // Get all of the top navigation links
     const links =
-        navigation.querySelectorAll(
-            ".main-navigation a"
-        );
+        navigation.querySelectorAll(".nav-links a");
 
 
-    // Highlight whichever page is currently open
     links.forEach(function (link) {
 
-        const linkPage =
-            link.getAttribute("href");
-
-
-        if (linkPage === currentPage) {
+        if (
+            link.getAttribute("href") === currentPage
+        ) {
 
             link.classList.add("active");
 
