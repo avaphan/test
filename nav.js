484ab0b63a645e999e8e32a4e79d3fca9ace666e
avaphan <aphan2@iastate.edu>
2026-09-10 08:55:22 -0500

    Refactor navigation menu and update link highlighting

--- nav.js
+++ nav.js
@@ -1,64 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    // Find the navigation area in the HTML
     const navigation =
         document.getElementById("navigation");
 
 
+    // If the page does not contain the navigation area,
+    // stop the script
     if (!navigation) {
         return;
     }
 
 
+    // Add the navigation menu
     navigation.innerHTML = `
-        <nav class="main-nav">
+        <nav class="main-navigation">
 
-            <div class="nav-brand">
+            <a href="index.html">
+                About Me
+            </a>
 
-                <a href="index.html">
-                    Ava Phan
-                </a>
+            <a href="research.html">
+                Research
+            </a>
 
-            </div>
-
-
-            <div class="nav-links">
-
-                <a href="index.html">
-                    About Me
-                </a>
-
-                <a href="research.html">
-                    Research
-                </a>
-
-                <a href="personal.html">
-                    Personal
-                </a>
-
-            </div>
+            <a href="personal.html">
+                Personal
+            </a>
 
         </nav>
     `;
 
 
+    // Find the name of the current page
     let currentPage =
         window.location.pathname.split("/").pop();
 
 
+    // GitHub Pages may show the homepage
+    // without displaying index.html
     if (currentPage === "") {
         currentPage = "index.html";
     }
 
 
+    // Get all of the top navigation links
     const links =
-        navigation.querySelectorAll(".nav-links a");
+        navigation.querySelectorAll(
+            ".main-navigation a"
+        );
 
 
+    // Highlight whichever page is currently open
     links.forEach(function (link) {
 
-        if (
-            link.getAttribute("href") === currentPage
-        ) {
+        const linkPage =
+            link.getAttribute("href");
+
+
+        if (linkPage === currentPage) {
 
             link.classList.add("active");
 
@@ -67,5 +67,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
-
-
